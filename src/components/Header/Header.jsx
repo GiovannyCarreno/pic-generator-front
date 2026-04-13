4de5a2bdb6ec4a6f319254e4601ccb8b07ad1a65
GiovannyCarreño <carrenoestupinanh@gmail.com>
2026-04-12 20:23:59 -0500

    Modificación en el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
     <div className="header">
-      <h1>Rock Art Image Generator</h1>
-      <p>Genera imágenes de pinturas rupestres usando redes neuronales generativas</p>
+      <h1>Generador de Arte Rupestre</h1>
+      <p>
+        Genera y reconstruye imágenes de pictogramas.
+      </p>
     </div>
   );
 }
